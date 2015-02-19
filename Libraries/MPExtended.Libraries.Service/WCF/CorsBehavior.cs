534e1ad60351044a3e41a7541b9d2d73724449d1
Alex Griffis <alexgriffis@gmail.com>
2015-02-19 11:53:07 -0800

    Add CORS Headers and fix erros to build in Visual Studio Community 2013
    PowerSheculerPlugin: Add reference to TVLibrary
    
    TvServerPlugin: Update PowerScheduler.Instance to TvEngine.PowerScheduler.PowerScheduler.Instance (Fixes a build error but this may be incorrect)
    
    Filesystem.wxs: Update RegistryKey element for depreciated 'Action' method
    
    App.config: Add corsBehaviorExtension to jsonBehavior
    
    Libraries.MPExtended.Libraries.Service -> WCF -> CorsBehavior.cs: Enables Cross Origin Resource Sharing for access to JSON endpoints by modern webapps.
    
    MPExtended.Libraries.Service.Config.csproj: Update sgen.exe path to correct build error.

---
+++ Libraries/MPExtended.Libraries.Service/WCF/CorsBehavior.cs
@@ -0,0 +1,83 @@
+﻿#region Copyright (C) 2012-2013 MPExtended
+// Copyright (C) 2012-2013 MPExtended Developers, http://www.mpextended.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
+
+namespace MPExtended.Libraries.Service.WCF
+{
+    internal class CorsMessageInspector : IDispatchMessageInspector
+    {
+        Dictionary<string, string> requiredHeaders;
+        public CorsMessageInspector(Dictionary<string, string> headers)
+        {
+            requiredHeaders = headers ?? new Dictionary<string, string>();
+        }
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            return null;
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            var httpHeader = reply.Properties["httpResponse"] as HttpResponseMessageProperty;
+            foreach (var item in requiredHeaders)
+            {
+                httpHeader.Headers.Add(item.Key, item.Value);
+            }
+        }
+    }
+
+    internal class CorsHeaderBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+        }
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            var requiredHeaders = new Dictionary<string, string>();
+
+            requiredHeaders.Add("Access-Control-Allow-Origin", "*");
+            requiredHeaders.Add("Access-Control-Request-Method", "POST,GET,PUT,DELETE,OPTIONS");
+            requiredHeaders.Add("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+
+            endpointDispatcher.DispatchRuntime.MessageInspectors.Add(new CorsMessageInspector(requiredHeaders));
+        }
+        public void Validate(ServiceEndpoint endpoint)
+        {
+        }
+    }
+
+    public class CorsBehavior : BehaviorExtensionElement
+    {
+        protected override object CreateBehavior() { return new CorsHeaderBehavior(); }
+        public override Type BehaviorType { get { return typeof(CorsHeaderBehavior); } }
+    }
+}
